3e4984b48244441971882c13ab04d63740d0526c
Kunal Chauhan <Kunal.chauhan19@comp.sce.edu.in>
2024-02-03 15:00:15 +0530

    .

--- jobapp/urls.py
+++ jobapp/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
+                  path('goodjob-admin/', admin.site.urls),
                   path('', include('app.urls')),
                   path('subscribe/', include('subscribe.urls')),
                   path('upload/', include('uploadapp.urls')),
