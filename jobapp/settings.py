c598d2b4694211fb03728e12ad8b4b46b2b7d19d
Kunal Chauhan <Kunal.chauhan19@comp.sce.edu.in>
2024-02-04 16:05:10 +0530

    updated: homepage, removed: logos

--- jobapp/settings.py
+++ jobapp/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('IS_DEVELOPMENT') == "TRUE"
 
-ALLOWED_HOSTS = [env("APP_HOST", "localhost", '127.0.0.1')]
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
